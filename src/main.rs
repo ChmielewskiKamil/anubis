d5b7be0ad20bb6354ea29651ee902f11fbd9e9a0
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-11-04 21:28:40 +0100

    👷 Find function name by walking the AST

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,40 @@ use cairo_lang_parser::db::ParserGroup;
 use cairo_lang_parser::printer::print_tree;
 use cairo_lang_filesystem::ids::FileId;
 use cairo_lang_diagnostics::DiagnosticsBuilder;
+use cairo_lang_syntax::node::kind::SyntaxKind::{FunctionWithBody, FunctionDeclaration, TokenIdentifier};
 use cairo_lang_parser::utils::{get_syntax_root_and_diagnostics_from_file, SimpleParserDatabase};
-use std::path::PathBuf;
+use std::{path::PathBuf, fs::File};
+use std::io::prelude::*;
 
 fn main() {
     let db = SimpleParserDatabase::default();
     let mut file_path = PathBuf::new();
-    file_path.push("test_files/example0.cairo");
+    file_path.push("test_files/example1.cairo");
     let file_id = FileId::new(&db, file_path.clone());
     let ast = db.file_syntax(file_id).unwrap().descendants(&db);
 
-    ast.for_each(|node| {
-        println!("{:?}", node.text(&db));
-        println!("{:?}", node.kind(&db));
-    });
+    let (syntax_root, _) = get_syntax_root_and_diagnostics_from_file(&db, file_path);
+    let ast_visual = print_tree(&db, &syntax_root, false, false); 
+    // Write the content to a markdown file
+    let mut file = File::create("test_files/ast_visual.md").unwrap();
+    file.write_all(ast_visual.as_bytes()).unwrap();
+
+    for node in ast {
+    if node.kind(&db) == FunctionWithBody {
+        // Traverse children of `FunctionWithBody` to find `FunctionDeclaration`
+        for child in node.descendants(&db) {
+            if child.kind(&db) == FunctionDeclaration {
+                // Now, find the `name` node within `FunctionDeclaration`
+                for name_child in child.descendants(&db) {
+                    if name_child.kind(&db) == TokenIdentifier {
+                        // This should be the function name
+                        println!("Function name: {:?}", name_child.text(&db).unwrap());
+                        break; // Break after finding the name
+                    }
+                }
+                break; // Break after processing `FunctionDeclaration`
+            }
+        }
+    }
+}
 }
