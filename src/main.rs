bfec5dc61abcc4c9e707638f5bb8e25e6437e1b4
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-11-04 20:46:45 +0100

    👷 Walk the AST

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use cairo_lang_parser::db::ParserGroup;
 use cairo_lang_parser::parser::Parser;
+use cairo_lang_parser::db::ParserGroup;
 use cairo_lang_parser::printer::print_tree;
+use cairo_lang_filesystem::ids::FileId;
+use cairo_lang_diagnostics::DiagnosticsBuilder;
 use cairo_lang_parser::utils::{get_syntax_root_and_diagnostics_from_file, SimpleParserDatabase};
 use std::path::PathBuf;
 
 fn main() {
     let db = SimpleParserDatabase::default();
     let mut file_path = PathBuf::new();
-    file_path.push("test_files/hello_world.cairo");
-    let (syntax_root, diagnostics) = get_syntax_root_and_diagnostics_from_file(&db, file_path);
-    let result = print_tree(&db, &syntax_root, false, false);
-    println!("{}", result);
+    file_path.push("test_files/example0.cairo");
+    let file_id = FileId::new(&db, file_path.clone());
+    let ast = db.file_syntax(file_id).unwrap().descendants(&db);
+
+    ast.for_each(|node| {
+        println!("{:?}", node.text(&db));
+        println!("{:?}", node.kind(&db));
+    });
 }
