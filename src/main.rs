9c70ff8bd200dca6af74b389061af94048723bdd
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-11-04 17:55:06 +0100

    👷 Quick mvp to see how AST works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use cairo_lang_parser::db::ParserGroup;
+use cairo_lang_parser::parser::Parser;
+use cairo_lang_parser::printer::print_tree;
+use cairo_lang_parser::utils::{get_syntax_root_and_diagnostics_from_file, SimpleParserDatabase};
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let db = SimpleParserDatabase::default();
+    let mut file_path = PathBuf::new();
+    file_path.push("test_files/hello_world.cairo");
+    let (syntax_root, diagnostics) = get_syntax_root_and_diagnostics_from_file(&db, file_path);
+    let result = print_tree(&db, &syntax_root, false, false);
+    println!("{}", result);
 }
