07200eb4832f57af77dd7081b220387272fd02e1
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-11-07 09:41:50 +0100

    👷 Extract the list of function parameters

--- src/analyser/qa/unused_parameter.rs
+++ src/analyser/qa/unused_parameter.rs
@@ -1,13 +1,16 @@
 use cairo_lang_parser::utils::SimpleParserDatabase;
 
-use cairo_lang_syntax::node::kind::SyntaxKind::FunctionWithBody;
+use cairo_lang_syntax::node::kind::SyntaxKind;
 use cairo_lang_syntax::node::SyntaxNode;
 
 use crate::analyser::ast::extract_target_from_node;
 
 pub fn unused_parameter_qa(source_code: SyntaxNode, db: &SimpleParserDatabase) {
-    let nodes = extract_target_from_node(FunctionWithBody, source_code, db);
-    for node in &nodes {
-        println!("Found a function with body: {:?}", node.kind(db));
-    }
+    // It will return a vector of SyntaxNodes of the ParamList kind
+    // from every single function in a file.
+    let params:Vec<SyntaxNode> = extract_target_from_node(SyntaxKind::ParamList, source_code, db);
+
+    params.iter().for_each(|param_group| {
+        println!("{:?}", param_group.clone().get_text_without_trivia(db));
+    })
 }
